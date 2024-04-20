94a1bdff356f88a8eebd5fe2dc69a74f93229fa5
FofuHupopo <fofuhupopo@gmail.com>
2024-04-20 15:06:06 +0500

    FIX: swagger url

--- universities/api/urls.py
+++ universities/api/urls.py
@@ -6,6 +6,6 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 urlpatterns = [
     path('university/', include("api.universities.urls")),
     
-    path('schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='docs')
+    path('university/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('university/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='docs')
 ]

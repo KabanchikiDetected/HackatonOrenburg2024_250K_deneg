6605482dc2da0617740630d25c162f63dcc34090
FofuHupopo <fofuhupopo@gmail.com>
2024-04-21 17:24:30 +0500

    add feed

--- frontend/src/pages/Feed/index.tsx
+++ frontend/src/pages/Feed/index.tsx
@@ -1,5 +1,18 @@
+import { Link } from "react-router-dom";
 import "./index.scss";
-import { useLayoutEffect, useState } from "react";
+import { useEffect, useState } from "react";
+
+interface IUser {
+  id: string
+  name: string,
+  last_name: string,
+  description: string,
+  birthday: "2007-04-14T00:00:00Z",
+  faculty_id: string,
+  photo: string,
+  role: string,
+  education?: string,
+}
 
 interface IPost {
   id: number,
@@ -8,27 +21,75 @@ interface IPost {
   author_id: string,
   created_at: number,
   likes: number,
-  images: string[] | string,
-  hashtags: string[] | string
+  images: string,
+  hashtags: string,
+  parsed_hashtags?: string[],
+  author?: IUser,
+  is_liked?: boolean,
 }
 
 const Feed = () => {
   const [token, _] = useState<string>(localStorage.getItem("token") as string);
   const [posts, setPosts] = useState<IPost[]>([])
 
-  useLayoutEffect(() => {
-    async function request() {
-      await fetch("/api/news/news/feed//", {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      })
-      .then(response => response.json())
-      .then(data => setPosts(data));
-    }
+  useEffect(() => {
+    loadPosts();
 
-    request();
   }, []);
+
+  const loadPosts = () => {
+    fetch('/api/news/feed/')
+    .then(response => response.json())
+    .then(data => {
+      Promise.all(
+        data.map((post: IPost) =>
+            fetch(`/api/students/${post.author_id}`)
+              .then(response => response.json())
+              .then(authorData => ({ ...post, author: {
+                ...authorData,
+                education: "No University",
+              }, parsed_hashtags: getHeshtags(post), is_liked: isLiked(post.id) }))
+        )
+      )
+      .then(postsData => {
+        Promise.all(
+          postsData.map(post =>
+            getEducation(post.author_id)
+              .then(education => ({ ...post, author: {
+                ...post.author,
+                education
+              }}))
+          )
+        )
+        .then(postsDataWithEducation => {
+          setPosts(postsDataWithEducation);
+        });
+      });
+    })
+    .catch(error => console.error('Error fetching news:', error));
+  }
+
+
+
+  const isLiked = async (postId: number) => {
+    if (token) {
+      try {
+        const response = await fetch(`/api/news/${postId}/like/`, {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        const data = await response.json();
+  
+        return data.liked;
+      } catch (error) {
+        console.error('Error fetching like status:', error);
+      }
+    }
+  
+    return false;
+  }
 
   const getDate = (created_at: number) => {
     const date = new Date(created_at * 1000);
@@ -40,6 +101,81 @@ const Feed = () => {
     const minutes = date.getMinutes().toString().padStart(2, '0');
 
     return `${day} ${month} ${hours}:${minutes}`;
+  }
+
+  const getHeshtags = (post: IPost) => {
+    return JSON.parse(post.hashtags);
+  }
+
+  const handleLikeToPost = (postId: number) => {
+    async function request(postId: number) {
+      try {
+        const response = await fetch(`/api/news/${postId}/like/`, {
+          method: "POST",
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        const data = await response.json();
+
+        if (data === "You have put a like") {
+          return { liked: true };
+        }
+        else if (data === "You removed the like") {
+          return { liked: false };
+        }
+
+      } catch (error) {
+        console.error('Error fetching like status:', error);
+      }
+
+      return { liked: false }
+    }
+
+    if (token) {
+      const responseData = request(postId);
+      responseData.then(data => {
+        setPosts([...posts.map(post => ({
+          ...post,
+          is_liked: data.liked,
+          likes: post.id === postId ? (data.liked === true ? post.likes + 1 : post.likes - 1) : post.likes,
+        }))])
+      })
+    }
+    else {
+      console.log("Not authorized")
+    }
+  }
+
+  const getImage = (post: IPost) => {
+    const images = JSON.parse(post.images);
+
+    if (images.length > 0) {
+      return images[0];
+    }
+    return "";
+  }
+
+  async function getEducation(userId: string) {
+    async function request(userId: string) {
+      try {
+        const response = await fetch(`/api/universities/user/education/${userId}/`, {
+          method: "GET"
+        });
+        const data = await response.json();
+        
+        return `${data.university.short_name}, ${data.department.short_name}, ${data.group.name}`
+        
+      } catch (error) {
+        console.error('Error fetching like status:', error);
+      }
+  
+      return "No University"
+    }
+    
+    const education = await request(userId);
+    
+    return await education
   }
 
   return (
@@ -54,18 +190,18 @@ const Feed = () => {
               <div className="about">
                 <div className="about__col col">
                   <div className="name">
-                    { post.author_id } <img src="/images/star.svg" alt="" /> <span>43</span>
+                    <Link to={`/user/${post.author_id}`} style={{ textDecoration: "none", color: "black" }}>{post.author?.name} {post.author?.last_name} </Link>
+                    <img src="/images/star.svg" alt="" /> <span>0</span>
                   </div>
                   <div className="date">{ getDate(post.created_at) }</div>
                 </div>
                 <div className="about__col col col-blue">
-                  КУБГУ, Экономический, Инноватика, 215Б
+                  <span>{ post.author?.education }</span>
                 </div>
               </div>
             </div>
             <div className="post__row row">
-              <img src="/images/post.png" alt="" />
-              <img src="/images/post.png" alt="" />
+              { getImage(post) && <img src={getImage(post)} alt="" /> }
             </div>
             <div className="post__col col">
               <strong className="title">
@@ -76,10 +212,13 @@ const Feed = () => {
               </div>
             </div>
             <div className="post__row row row-space">
-              <button className="like">
-                <img src="/images/like.svg" alt="" /> <span> {post.likes} </span>
-              </button>
-              <p className="blue-bold">Творчество</p>
+              <div>
+                <button className="like" onClick={() => handleLikeToPost(post.id)}>
+                  <img src="/images/like.svg" alt="" />
+                </button>
+                <span> {post.likes} </span>
+              </div>
+              <p className="blue-bold">{ post?.parsed_hashtags ? post?.parsed_hashtags[0] : "" }</p>
             </div>
           </div>
         )) }
